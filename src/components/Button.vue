378a6ca8149c9a31fa74ce77fb921bb089b82d6f
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2021-07-28 15:04:10 +0200

    Initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,16 @@
+<template name="Button">
+  <button @click="onClick()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {},
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template name="Header">
+  <header>
+    <h1>{{ heading }}</h1>
+    <Button text="Add Task" color="green" />
+  </header>
+</template>
+
+<script>
+import Button from './Button.vue';
+export default {
+  name: 'Header',
+  props: {
+    heading: {
+      heading: String,
+      default: 'Hello',
+    },
+  },
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
