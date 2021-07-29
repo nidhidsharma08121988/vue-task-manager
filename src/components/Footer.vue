2f38af2515bbf7578875404b2ec3c4e7a573c405
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2021-07-29 13:14:40 +0200

    Implement routing

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template name="Footer">
+  <footer>
+    <p>copyright &copy; 2021</p>
+    <router-link to="/about">About</router-link>
+  </footer>
+</template>
+<style scoped>
+a {
+  color: #333;
+}
+footer {
+  margin-top: 30px;
+  text-align: center;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template name="About">
+  <div>
+    <h3>About us</h3>
+    <p>Version 1.0.0</p>
+    <router-link to="/">Go Back</router-link>
+  </div>
+</template>
